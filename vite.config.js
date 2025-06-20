b037a4c8243c9b74b08c6fef6b5c6787c073e485
Soham M <soham_m@Sohams-MacBook-Air.local>
2025-06-20 17:26:24 +0530

    Fix: set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/vue-games/',
+  base: '/vue-games/',
   plugins: [
     vue(),
     vueDevTools(),
